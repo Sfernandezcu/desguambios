4aeed23481a2b330b9e11ac8a736f72b89fbe355
SFernandez <sergioarizmendi43@gmail.com>
2018-01-25 12:22:01 +0100

    original

---
+++ Desguambios/src/main/java/com/dsg/desguambios/DesguambiosApplication.java
@@ -0,0 +1,12 @@
+package com.dsg.desguambios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesguambiosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesguambiosApplication.class, args);
+	}
+}
---
+++ Desguambios/src/test/java/com/dsg/desguambios/DesguambiosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.dsg.desguambios;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DesguambiosApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
