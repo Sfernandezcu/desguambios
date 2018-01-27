d70b4957537d0ea39a0cb9b4df3682ffafbf0e17
SFernandez <sergioarizmendi43@gmail.com>
2018-01-27 12:01:27 +0100

    prueba commit

--- Desguambios/src/main/java/com/dsg/desguambios/subidaporguillermino.java
+++ Desguambios/src/main/java/com/dsg/desguambios/subidaporguillermino.java
@@ -1,31 +1,5 @@
 package com.dsg.desguambios;
 
 public class subidaporguillermino {
-	
-	
-	public void hola() {
-		System.out.print("hola");
-	}
-	
-<<<<<<< HEAD
-	//cambio de las 11:24
-	// cambio de las 11:25
-	//cambio de las 11:37
-	
-	
-	//cambio para probar otra vez
-	
-
-	//cambio sergio 11:43
-
-	
-	//cambio otro para probar conflictos
-	
-	// probar cambio local
-
-	// probamos otro cambio a las 11:55
-=======
-
->>>>>>> branch 'master' of https://github.com/di3guez/desguambios
 
 }
