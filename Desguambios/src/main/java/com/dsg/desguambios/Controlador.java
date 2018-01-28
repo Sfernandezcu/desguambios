bf8eda3e5dacdc7821b1cd568412b792c667c083
Guillermo <Guillermo@DESKTOP-NDVH0EH>
2018-01-28 21:01:00 +0100

    Clase Controlador y UsersService

---
+++ Desguambios/src/main/java/com/dsg/desguambios/Controlador.java
@@ -0,0 +1,23 @@
+package com.dsg.desguambios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+@Controller
+public class Controlador {
+
+	@Autowired
+	 private UsersService usersService;
+	
+	 @RequestMapping("/nombre_url")
+	 public String greeting(Model model) {
+	 model.addAttribute("name",
+	 usersService.getNumUsers()+" users");
+
+	 return "prueba";
+	 }
+
+}
---
+++ Desguambios/src/main/java/com/dsg/desguambios/UsersService.java
@@ -0,0 +1,9 @@
+package com.dsg.desguambios;
+
+public class UsersService {
+
+	public int getNumUsers(){
+		 return 5;
+		 }
+
+}
