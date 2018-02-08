39e0b57981dd536ce67363d67c3bbc810e80d01a
daniel <daniel@localhost>
2018-02-08 12:15:18 +0100

    Dependencias jpa solucionadas

--- Desguambios/src/main/java/com/dsg/desguambios/Producto.java
+++ Desguambios/src/main/java/com/dsg/desguambios/Producto.java
@@ -1,6 +1,10 @@
 package com.dsg.desguambios;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 
 @Entity
 public class Producto {
