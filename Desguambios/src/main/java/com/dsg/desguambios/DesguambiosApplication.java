2f7a328ad795f163f26c8dfe1248dbadcb37e84b
S.fernandezcu <sergioarizmendi43@gmail.com>
2018-02-11 21:00:56 +0100

    quitado import

--- Desguambios/src/main/java/com/dsg/desguambios/DesguambiosApplication.java
+++ Desguambios/src/main/java/com/dsg/desguambios/DesguambiosApplication.java
@@ -2,11 +2,10 @@ package com.dsg.desguambios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
+
 
 @SpringBootApplication
 public class DesguambiosApplication {
-	
 	
 
 	public static void main(String[] args) {
