2987f74c6ae0a2de07dd45d96cee3837dab285fd
daniel <daniel@localhost>
2018-02-14 21:23:46 +0100

    Crear entidad Usuario

--- Desguambios/src/main/java/com/dsg/desguambios/entidades/Usuario.java
+++ Desguambios/src/main/java/com/dsg/desguambios/entidades/Usuario.java
@@ -2,11 +2,22 @@ package com.dsg.desguambios.entidades;
 
 import java.util.ArrayList;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.SessionScope;
 @Component
 @SessionScope
+@Entity
 public class Usuario {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private long idUsuario;
+	
+	
 	
 	private ArrayList<Producto> listaFavoritos;
 	
