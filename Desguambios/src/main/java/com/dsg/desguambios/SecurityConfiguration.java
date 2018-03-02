623b1ad75f383642f560e1c53160445abd23c6f1
daniel <daniel@localhost>
2018-03-02 16:47:40 +0100

    CSRF handler

--- Desguambios/src/main/java/com/dsg/desguambios/SecurityConfiguration.java
+++ Desguambios/src/main/java/com/dsg/desguambios/SecurityConfiguration.java
@@ -36,8 +36,6 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.logout().logoutUrl("/logout"); 
         http.logout().logoutSuccessUrl("/");
         
-        // Disable CSRF at the moment
-        http.csrf().disable();
     }
 
     @Override
