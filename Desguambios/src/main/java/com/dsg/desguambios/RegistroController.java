d549c64f7651ff5b3b419ed5fbcd79024482d6c2
S.fernandezcu <sergioarizmendi43@gmail.com>
2018-03-08 12:34:47 +0100

    añadiendo envio de correo registro

--- Desguambios/src/main/java/com/dsg/desguambios/RegistroController.java
+++ Desguambios/src/main/java/com/dsg/desguambios/RegistroController.java
@@ -9,10 +9,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
 import com.dsg.desguambios.entidades.Comentario;
 import com.dsg.desguambios.entidades.Desguace;
@@ -60,12 +63,28 @@ public class RegistroController {
 			 public String registro(Model model,@RequestParam String alta, String usuario,String email,
 					  String direccion,  String password, String valPassword) {
 				
+				 RestTemplate restTemplate = new RestTemplate();
+				 String URL="http://localhost:8080/";
+				 
+				 MultiValueMap<String, String> correo= new LinkedMultiValueMap<String, String>();
+			        correo.add("email", email);
+			        correo.add("subject", "Registro Satisfactorio");
+			        correo.add("body", " Su registro ha sido realizado Correctamente ");
+			        
+			      
+			   
+				 
+				 
 				if(Integer.parseInt(alta)==1) {
 					Desguace instanciaDesguace = new Desguace(usuario,email,direccion,password,valPassword);
 					desguaceRepository.save(instanciaDesguace);
 					//lista.add(desguace);
 					//lista.add(desguace1);
 					model.addAttribute("mensaje", true);
+					
+					
+					//enviar correo confirmacion de registro
+					 restTemplate.postForLocation(URL, correo);
 					
 				} else {
 					model.addAttribute("mensaje", false);
